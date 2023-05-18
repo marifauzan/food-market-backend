5de22feec598dcf48ca78d7b03f408f06863cf90
Mar'i Fauzan <marifauzan20@gmail.com>
2023-05-18 23:05:45 +0700

    Styling detail transactions and adding delete function in transaction controller

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -84,9 +84,11 @@ class TransactionController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Transaction $transaction)
     {
-        //
+        $transaction->delete();
+
+        return redirect()->route('transactions.index');
     }
 
     public function changeStatus(Request $request, $id, $status)
